621894ddc646e31f84f737211591e15ae6100189
DavidPToyo <davidpichardot31@hotmail.com>
2025-06-27 13:25:14 +0000

    desafio listo

---
+++ apoyo_desafio.py
@@ -0,0 +1,33 @@
+from error import DimensionError
+
+class Foto():
+    MAX = 2500
+
+    def __init__(self, ancho: int, alto: int, ruta: str) -> None:
+        self.__ancho = ancho
+        self.__alto = alto
+        ruta = ruta
+
+    @property
+    def ancho(self) -> int:
+        return self.__ancho
+
+    @ancho.setter
+    def ancho(self, ancho) -> None:
+        # Si el ancho es mayor a 1 o menor que el maximo
+        if ancho > self.MAX or ancho < 1:
+            DimensionError("DJSJAKHD", ancho, self.MAX)
+        else:
+            self.__ancho = ancho
+        
+
+    @property
+    def alto(self) -> int:
+        return self.__alto
+
+    @alto.setter
+    def alto(self, alto) -> None:
+        if 1 <= alto <= self.MAX:
+            self.__alto = alto
+        else:
+            raise DimensionError("MENSAJE", alto, self.MAX)                    
